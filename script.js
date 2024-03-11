436265b77ba33f44d35bf4a731bca1f50ca1d92d
snc29654 <snc29654l@nifty.com>
2024-03-11 09:39:19 +0900

    change file_write

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-//alert("script.js successfully loaded");
+$(function(){
+    
 
-$(function () {
-   const pickerOpts = {
-     types: [
-       {
-         description: 'Texts(.txt)',
-         accept: {
-           'text/*': ['.txt']
-         }
-       }
-     ],
-     multiple: false,
-   }
- 
-   let fileReader = new FileReader();
-   let start = 0;
-   let end = 0;
-   $("#load").click(async function () {
- 
-     [fileHandle] = await window.showOpenFilePicker(pickerOpts);
- 
-     $("#message").html("Loading...");
-     start = performance.now();			//track start
-     const file = await fileHandle.getFile();
-     const fileContents = await file.text();
-     console.log(fileContents);
-     $("#log").val(fileContents);
- 
-     end = performance.now();			//track end
-     const time = (end - start) | 0;
-     $("#message").html("Data is successfully loaded in " + (time / 1000) + "s");
-   });
- 
-   $("#save").click(async function () {
-     let fileHandle = await window.showSaveFilePicker(pickerOpts);
-     const writable = await fileHandle.createWritable();
-     await writable.write($("#log").val());
-     await writable.close();
-   });
- 
- });
+    download_txt("test.txt", "12345");
+
+    function download_txt(file_name, data) {
+
+        const blob = new Blob([data], {type: 'text/plain'});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.download = file_name;
+        a.href = url;
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(url);
+
+    }
+});
